903cc11701e7736fdafa5ba1f44b2a378f29a3b8
rotomeca <delphin.tommy@gmail.com>
2022-03-07 18:08:22 +0100

    Ajout d'une classe d'aide pour la gestion de l'aléatoire

---
+++ Classes/Aleatoire.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RotomecaLib
+{
+    public static class Aleatoire
+    {
+        static Random _random = null;
+        public static Random Random
+        {
+            get
+            {
+                if (_random == null) _random = new Random();
+                return _random;
+            }
+        }
+
+        public static int Nombre(int min, int max, int seed)
+        {
+            Random random = new Random(seed);
+            return random.Next(min, max);
+        }
+
+        public static int Nombre(int min, int max)
+        {
+            return Random.Next(min, max);
+        }
+
+        public static double Nombre(int seed)
+        {
+            Random random = new Random(seed);
+            return random.NextDouble();
+        }
+
+        public static double Nombre()
+        {
+            return Random.NextDouble();
+        }
+    }
+}
